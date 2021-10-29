73bf542efb04a5667b7430afa4096cb4e2dbae97
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-28 22:41:38 -0500

    feat: log the user in

--- web/app.py
+++ web/app.py
@@ -10,14 +10,14 @@ from web.routes.appointments import appointments_blueprint
 from web.routes.users import users_blueprint
 
 app = Flask(__name__)
-app.config.from_object(BaseConfig.SQLALCHEMY_DATABASE_URI)
+# app.config.from_object(BaseConfig.SQLALCHEMY_DATABASE_URI)
 db = SQLAlchemy(app)
 
 app.register_blueprint(appointments_blueprint)
 app.register_blueprint(users_blueprint)
 app.register_blueprint(auth_blueprint)
 
-engine = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
+# engine = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
